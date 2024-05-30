bf58178e2adfcbe2b3ffd13f7c702e6768a7808c
DmitryGorodenskiy <vlad.gorod.69@yandex.ru>
2024-05-30 12:51:16 +0300

    Отображение пульта

--- src/java/gorodenskiy/commande/Main.java
+++ src/java/gorodenskiy/commande/Main.java
@@ -1,2 +1,10 @@
-package gorodenskiy.commande;public class Main {
+package gorodenskiy.commande;
+
+import gorodenskiy.commande.graphics.Pult;
+
+public class Main {
+    public static void main(String[] args) {
+        Supervisor supervisor = new Supervisor();
+        supervisor.start();
+    }
 }
---
+++ src/java/gorodenskiy/commande/Supervisor.java
@@ -0,0 +1,12 @@
+package gorodenskiy.commande;
+
+import gorodenskiy.commande.graphics.Pult;
+
+public class Supervisor {
+    private String[] textFieldStrings = new String[] {"Свет в доме", "Гараж", "Подвал", "Парковка", "Мастерская", "Теплица 1", "Теплица 2"};
+    private int buttonsCount = 9;
+    public void start() {
+        Pult pult = new Pult(textFieldStrings, buttonsCount);
+        pult.myCreateAndShowAPI(pult);
+    }
+}
