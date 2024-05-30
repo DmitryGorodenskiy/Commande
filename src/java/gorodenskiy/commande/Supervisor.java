c210dcfbca608b6ee7b08a8144dee7d2422f4b70
DmitryGorodenskiy <vlad.gorod.69@yandex.ru>
2024-05-30 14:38:02 +0300

    Новый объект команды

--- src/java/gorodenskiy/commande/Supervisor.java
+++ src/java/gorodenskiy/commande/Supervisor.java
@@ -2,17 +2,31 @@ package gorodenskiy.commande;
 
 import gorodenskiy.commande.graphics.IPrinter;
 import gorodenskiy.commande.graphics.Pult;
+import gorodenskiy.commande.sandbox.Light;
+import gorodenskiy.commande.sandbox.LightOnCommand;
+import gorodenskiy.commande.sandbox.SimpleRemoteControl;
 
 public class Supervisor implements IPrinter {
     private String[] textFieldStrings = new String[] {"Свет в доме", "Гараж", "Подвал", "Парковка", "Мастерская", "Теплица 1", "Теплица 2"};
     private Pult pult;
-    private int buttonsCount = 9;
-    public Supervisor() {pult = new Pult(textFieldStrings, buttonsCount, this);}
+    private int buttonsCount = 1;
+    private SimpleRemoteControl remoteControl;
+    public Supervisor() {
+        remoteControl = new SimpleRemoteControl();
+        pult = new Pult(textFieldStrings, buttonsCount, this);
+    }
     @Override
-    public void print(String message) {pult.setTextOut(message);}
+    public void print(String message) {
+        //pult.setTextOut(message);
+        remoteControl.buttonWasPressed();
+    }
     public void start(){
         pult.setTextOut("Проба\nПривет Мир!");
         pult.myCreateAndShowAPI(pult);
         pult.setTextOut("Новая проба прошла!");
+
+        Light light = new Light(pult);
+        LightOnCommand lightOnCommand = new LightOnCommand(light);
+        remoteControl.setCommand(lightOnCommand);
     }
 }
